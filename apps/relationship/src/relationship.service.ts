7d421e3794ab634e6544efabdfa90c027eae62b5
Pasonnn <pason.dev@gmail.com>
2025-09-10 22:37:45 +0700

    add: app structure

--- apps/relationship/src/relationship.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class RelationshipService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
---
+++ apps/relationship/src/schemas/user.schema.ts
@@ -0,0 +1,51 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true, collection: 'users' })
+export class User extends Document {
+  @Prop({ required: true, unique: true, index: true })
+  username: string;
+
+  @Prop({ required: true, unique: true, index: true })
+  email: string;
+
+  @Prop({ required: false })
+  display_name: string;
+
+  @Prop({ required: true })
+  password_hashed: string;
+
+  @Prop({ required: false, default: 'Hi, i am a new Decode User' })
+  bio: string;
+
+  @Prop({
+    required: false,
+    default: 'bafkreibmridohwxgfwdrju5ixnw26awr22keihoegdn76yymilgsqyx4le',
+  })
+  avatar_ipfs_hash: string;
+
+  @Prop({
+    required: false,
+    default:
+      'https://res.cloudinary.com/dfzu1b238/image/upload/v1748419831/default_user_icon_rt4zcm.png',
+  })
+  avatar_fallback_url: string;
+
+  @Prop({
+    required: false,
+    default: 'user',
+    enum: ['user', 'admin', 'moderator'],
+  })
+  role: string;
+
+  @Prop({ required: false, default: null, type: Date })
+  last_login: Date | null;
+
+  @Prop({ required: false, default: null, type: Date })
+  last_username_change: Date | null;
+
+  @Prop({ required: false, default: null, type: Date })
+  last_email_change: Date | null;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
