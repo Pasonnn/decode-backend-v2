c7e847dbff74bffa90b2bd43e4594b8da4674ee4
Pasonnn <pason.dev@gmail.com>
2025-09-23 03:48:30 +0700

    add: cloud email worker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,12 +8,10 @@ module.exports = {
       name: 'auth',
       script: 'dist/apps/auth/main.js',
     },
-    /**
     {
       name: 'email-worker',
       script: 'dist/apps/email-worker/main.js',
     },
-    */
     {
       name: 'user',
       script: 'dist/apps/user/main.js',
