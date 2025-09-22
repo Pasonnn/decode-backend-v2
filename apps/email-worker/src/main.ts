ab51adf3f43747b3094576f6b70cc82f568eac23
Pasonnn <pason.dev@gmail.com>
2025-09-22 18:38:19 +0700

    fix: use configService for RMQ import in email-worker

--- apps/email-worker/src/main.ts
+++ apps/email-worker/src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { EmailWorkerModule } from './email-worker.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(EmailWorkerModule, {
     transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URI || 'amqp://localhost:5672'],
-      queue: 'email_queue',
-      queueOptions: {
-        durable: true,
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      options: {
+        urls: [
+          configService.get<string>('RABBITMQ_URI') || 'amqp://localhost:5672',
+        ],
+        queue: 'email_queue',
+        queueOptions: {
+          durable: true,
+        },
       },
-    },
+    }),
+    inject: [ConfigService],
   });
 
   await app.listen();
