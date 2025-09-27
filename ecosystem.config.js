aa8c34789c4f94d897b23cb9a349b27dec315a37
Pasonnn <pason.dev@gmail.com>
2025-09-27 19:35:22 +0700

    split email service to other cloud comp

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,6 @@ module.exports = {
       script: 'dist/apps/auth/main.js',
     },
     {
-      name: 'email-worker',
-      script: 'dist/apps/email-worker/main.js',
-    },
-    {
       name: 'user',
       script: 'dist/apps/user/main.js',
     },
