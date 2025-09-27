12f3147042d6757a001c3b6c792c44bfe8e3aa9f
Pasonnn <pason.dev@gmail.com>
2025-09-27 16:53:49 +0700

    fix api endpoints config

--- apps/api-gateway/src/config/configuration.ts
+++ apps/api-gateway/src/config/configuration.ts
@@ -14,31 +14,31 @@ export default () => ({
       url:
         process.env.AUTH_HOST && process.env.AUTH_PORT
           ? `http://${process.env.AUTH_HOST}:${process.env.AUTH_PORT}`
-          : 'http://localhost:4001',
+          : 'localhost:4001',
     },
     user: {
       url:
         process.env.USER_HOST && process.env.USER_PORT
           ? `http://${process.env.USER_HOST}:${process.env.USER_PORT}`
-          : 'http://localhost:4002',
+          : 'localhost:4002',
     },
     email: {
       url:
         process.env.EMAIL_HOST && process.env.EMAIL_PORT
           ? `http://${process.env.EMAIL_HOST}:${process.env.EMAIL_PORT}`
-          : 'http://localhost:4003',
+          : 'localhost:4003',
     },
     relationship: {
       url:
         process.env.RELATIONSHIP_HOST && process.env.RELATIONSHIP_PORT
           ? `http://${process.env.RELATIONSHIP_HOST}:${process.env.RELATIONSHIP_PORT}`
-          : 'http://localhost:4004',
+          : 'localhost:4004',
     },
     wallet: {
       url:
         process.env.WALLET_HOST && process.env.WALLET_PORT
           ? `http://${process.env.WALLET_HOST}:${process.env.WALLET_PORT}`
-          : 'http://localhost:4005',
+          : 'localhost:4005',
     },
     notification: {
       url:
