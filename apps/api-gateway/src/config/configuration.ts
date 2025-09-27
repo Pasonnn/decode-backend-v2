28a83f92b426324acef8d64ea031d00ca8b24dfd
Pasonnn <pason.dev@gmail.com>
2025-09-27 17:06:20 +0700

    fix endpoint host config

--- apps/api-gateway/src/config/configuration.ts
+++ apps/api-gateway/src/config/configuration.ts
@@ -14,37 +14,37 @@ export default () => ({
       url:
         process.env.AUTH_HOST && process.env.AUTH_PORT
           ? `http://${process.env.AUTH_HOST}:${process.env.AUTH_PORT}`
-          : 'localhost:4001',
+          : 'http://localhost:4001',
     },
     user: {
       url:
         process.env.USER_HOST && process.env.USER_PORT
           ? `http://${process.env.USER_HOST}:${process.env.USER_PORT}`
-          : 'localhost:4002',
+          : 'http://localhost:4002',
     },
     email: {
       url:
         process.env.EMAIL_HOST && process.env.EMAIL_PORT
           ? `http://${process.env.EMAIL_HOST}:${process.env.EMAIL_PORT}`
-          : 'localhost:4003',
+          : 'http://localhost:4003',
     },
     relationship: {
       url:
         process.env.RELATIONSHIP_HOST && process.env.RELATIONSHIP_PORT
           ? `http://${process.env.RELATIONSHIP_HOST}:${process.env.RELATIONSHIP_PORT}`
-          : 'localhost:4004',
+          : 'http://localhost:4004',
     },
     wallet: {
       url:
         process.env.WALLET_HOST && process.env.WALLET_PORT
           ? `http://${process.env.WALLET_HOST}:${process.env.WALLET_PORT}`
-          : 'localhost:4005',
+          : 'http://localhost:4005',
     },
     notification: {
       url:
         process.env.NOTIFICATION_HOST && process.env.NOTIFICATION_PORT
           ? `http://${process.env.NOTIFICATION_HOST}:${process.env.NOTIFICATION_PORT}`
-          : 'http://localhost:4006',
+          : 'http://0.0.0.0:4006',
     },
   },
 });
